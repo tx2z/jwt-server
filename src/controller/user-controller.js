968cb2474d6e9ccb49f2cbaa9adeb0f1a0efc823
tx2z <tx2z@caosmental.com>
2019-01-07 02:44:00 +0100

    Connecto to hasura server to create new users

--- src/controller/user-controller.js
+++ src/controller/user-controller.js
@@ -1,10 +1,32 @@
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
+var request = require('request');
 const config = require('../environments/environments')[process.env.NODE_ENV || 'development'];
 
+
 function createToken(user) {
-    return jwt.sign({ id: user.id, email: user.email }, config.JWTSECRET, {
-        expiresIn: 200 // 86400 expires in 24 hours
+    let credentials;
+    if (user == config.HASURA_ROLE) {
+        credentials = {
+            'https://hasura.io/jwt/claims': {
+                'x-hasura-default-role': user,
+                'x-hasura-allowed-roles': [user],
+            }
+        };
+    } else {
+        credentials = {
+            id: user.id,
+            email: user.email,
+            'https://hasura.io/jwt/claims': {
+                'x-hasura-user-id': (user.hasuraid).toString(),
+                'x-hasura-default-role': 'user',
+                'x-hasura-allowed-roles': ['user'],
+
+            }
+        };
+    }
+    return jwt.sign(credentials, config.JWTSECRET, {
+        expiresIn: 86400 // 86400 expires in 24 hours
     });
 }
 
@@ -22,13 +44,46 @@ exports.registerUser = (req, res) => {
             return res.status(400).json({ 'msg': 'The user already exists' });
         }
 
-        let newUser = User(req.body);
-        newUser.save((err, user) => {
+        // Call Hasura API to create user
+        const token = createToken(config.HASURA_ROLE);
+        let data = {
+            query: 'mutation insert_user{insert_user(objects: [{name:"' + req.body.email +'"}]) {returning {id}}}',
+            variables: null,
+            operationName: "insert_user"
+        };
+
+        request.post({
+            headers: {
+                'content-type': 'application/json', 'Authorization': 'Bearer ' + token
+            },
+            url: config.HASURA_URI,
+            json: data
+        }, function (err, httpResponse, body) {
             if (err) {
-                return res.status(400).json({ 'msg': err });
+                console.log(err);
+                return res.status(400).json({ 'msg': 'Error creating user in Hasura' });
+            } else {
+                if (body.data.insert_user.returning[0].id) {
+                    // console.log(body);
+                    // console.log(body.data.insert_user.returning[0].id);
+                    req.body.hasuraid = body.data.insert_user.returning[0].id;
+
+                    let newUser = User(req.body);
+                    newUser.save((err, user) => {
+                        if (err) {
+                            return res.status(400).json({ 'msg': err });
+                        }
+                        return res.status(201).json(user);
+                    });
+                } else {
+                    // console.log(body);
+                    return res.status(400).json({ 'msg': 'Error creating user in Hasura' });
+                }
             }
-            return res.status(201).json(user);
-        });
+        }); 
+
+
+        
     });
 };
 
